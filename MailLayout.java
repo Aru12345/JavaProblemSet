d7331ad276c60c7075a8fb009ca8b1e2f31661b1
Arundhati <arundhatiraicar12@gmail.com>
2024-04-04 14:36:00 -0400

    Traffic Prob

--- MailLayout.java
+++ MailLayout.java
@@ -8,10 +8,11 @@ import java.awt.event.*;
 import java.io.*;    
 
 /*
- *The class demonstrates creating a Maillayout using GUI.
+ *The class demonstrates creating a Mail layout using GUI.
  */
 public class MailLayout {
-    //The main method instantiates an instance of a class that extends JFrame.
+
+    //The main method contructs a window.
     public static void main(String[] args) {
         String title = "New Message";
         MyWindow myWindow = new MyWindow(title);
@@ -20,7 +21,7 @@ public class MailLayout {
 }
 
 /*
- *The class's constructor handles the layout and logic of the program
+ *The class extends Jframe.
  */
 class MyWindow extends JFrame {
 
@@ -43,24 +44,24 @@ class MyWindow extends JFrame {
     private JTextField textfield3 = new JTextField("");
     private JTextField textfield4 = new JTextField("");
     private JTextArea area = new JTextArea();
-    String names[] = { "Koshi Raicar <koshiraicar@gmail.com>", "Kathy Raicar <kathyraicar@gmail.com",
-            "Mantra Raicar <mantrasyda@gmail.com>" };
+    String names[] = { "Koshi Raicar <koshiraicar@gmail.com>", "Kathy Raicar <kathyraicar@gmail.com", "Mantra Raicar <mantrasyda@gmail.com>" };
     JComboBox<String> cb = new JComboBox<>(names);
   
     Font f = new Font("SansSerif", Font.PLAIN, 15);
     Dimension textFieldSize = new Dimension(150, 18);
    
+    // The actual creation of the window happens in the constructor
     public MyWindow() {}
-     public MyWindow(String title) {
+    public MyWindow(String title) {
         super(title);
-        layoutComponents(title);
+
+        layoutComponents();
         addListeners();
     }
 
-     private void layoutComponents(String title) {
-      
+    //The method sets up the windows appearance.
+    private void layoutComponents() {
         
-
         myPanel3.setLayout(new BoxLayout(myPanel3, BoxLayout.Y_AXIS));
         myPanel3.add(button);
 
@@ -101,183 +102,55 @@ class MyWindow extends JFrame {
         this.setSize(500, 400);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     }
+    }
     
-      private void addListeners() {
-         String fileName ="outbox.txt";
-         JTextArea areaMsg = this.area;
+    //The method set's up the window's plumbing(Listeners are connected to the widgets.)
+    private void addListeners() {
+        String fileName ="outbox.txt";
+        JTextArea areaMsg = this.area;
         
         textfield4.addFocusListener(new FocusListener() {
-        public void focusGained(FocusEvent e) {
-            // Implement if needed
-        }
+            
+            //No action takes place during focus Gain
+            public void focusGained(FocusEvent e) {
+            }
 
-        public void focusLost(FocusEvent e) {
-            String subject = textfield4.getText();
-            setTitle(subject.isEmpty() ? "New Message" : subject);
-        }
-    });
-
-        button.addActionListener(new ActionListener(){
-                 public void actionPerformed(ActionEvent e){
-            sendOperation(e,fileName,areaMsg);
-                 }
+            //Title is updated to what user enters in subject textfield after focus is lost
+            public void focusLost(FocusEvent e) {
+                String subject = textfield4.getText();
+                setTitle(subject.isEmpty() ? "New Message" : subject);
+            }
 
         });
 
-      }
+        button.addActionListener(new ActionListener(){
 
-      /*   private void updateTitle(ActionEvent e) {
-             focusGained(e);
-             focusLost(e);
-         
-            
-        }*/
-       //  private void focusGained(FocusEvent e) {}
-
-        //When focus is lost the title is updated using submethod.
-       /*  private void focusLost(FocusEvent e) {
-        String subject = textfield4.getText();
-        setTitle(subject.isEmpty() ? "New Message" :  subject); 
-        }*/
-
-        private void sendOperation(ActionEvent e,String file,JTextArea aj){
-             try
-        {
-            PrintWriter outStream =  new PrintWriter (new File (file));
-            outStream.print (aj.getText());
-            outStream.close ();
-
-            setTitle("New Message");
-            textfield1.setText("");
-            textfield2.setText("");
-            textfield3.setText("");
-            textfield4.setText("");
-            area.setText("");
-        }
-        catch (IOException o)
-        {
-            aj.setText("IOERROR: " + o.getMessage() + "\n");
-            o.printStackTrace();
-        }
-
-        }
-        
-        
-    
-
-}
-/*
-  void focusGained(FocusEvent e) {}
-
-        //When focus is lost the title is updated using submethod.
-         void focusLost(FocusEvent e) {
-            String subject = textfield4.getText();
-            setTitle(subject.isEmpty() ? "New Message" :  subject);   
-        }
-public MyWindow(String title) {
-
-        super(title);
-
-        myPanel3.setLayout(new BoxLayout(myPanel3, BoxLayout.Y_AXIS));
-        myPanel3.add(button);
-
-        myPanel1.setLayout(new BoxLayout(myPanel1, BoxLayout.Y_AXIS));
-        myPanel1.add(label1);
-        label1.setFont(f);
-        myPanel1.add(label2);
-        label2.setFont(f);
-        myPanel1.add(label3);
-        label3.setFont(f);
-        myPanel1.add(label4);
-        label4.setFont(f);
-        myPanel1.add(label5);
-        label5.setFont(f);
-
-        myPanel11.setLayout(new BorderLayout());
-
-        myPanel2.setLayout(new BoxLayout(myPanel2, BoxLayout.Y_AXIS));
-        myPanel2.add(textfield1);
-        myPanel2.add(textfield2);
-        myPanel2.add(textfield3);
-        myPanel2.add(textfield4);
-        myPanel2.add(cb);
-
-        textfield1.setPreferredSize(textFieldSize);
-        textfield2.setPreferredSize(textFieldSize);
-        textfield3.setPreferredSize(textFieldSize);
-        textfield4.setPreferredSize(textFieldSize);
-        
-        myPanel12.setLayout(new BorderLayout());
-
-        myPanel11.add(myPanel1, BorderLayout.WEST);
-        myPanel11.add(myPanel3, BorderLayout.NORTH);
-        myPanel11.add(myPanel2, BorderLayout.CENTER);
-
-        textfield4.addFocusListener(new fListner());
-        button.addActionListener(new SendEvent());
-
-        this.add(myPanel11, BorderLayout.NORTH);
-        this.add(area, BorderLayout.CENTER);
-        this.setSize(500, 400);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //The method uses submethod to perform the certain operations.
+            public void actionPerformed(ActionEvent e){
+                sendOperation(e,fileName,areaMsg);
+            }
+        });
     }
 
-    //The class's constructor is carrying out the operation of reading the Subject Textfield and setting it to the JFrames title 
-    class fListner implements FocusListener{
-
-        //The method Does nothing when focus is gained   
-        public void focusGained(FocusEvent e) {}
-
-        //When focus is lost the title is updated using submethod.
-        public void focusLost(FocusEvent e) {
-            updateTitle();   
-        }
-    }
-
-    //The class provides functionality for handling action events.
-    class SendEvent implements ActionListener{
-
-        //The method resets the textfields after performing the operation of sending the mail.
-        public void actionPerformed(ActionEvent event){
-            writeTextFile (area, "outbox.txt");
-            setTitle("New Message");
-            textfield1.setText("");
-            textfield2.setText("");
-            textfield3.setText("");
-            textfield4.setText("");
-            area.setText("");
-        }
-    }
-
-    //The method writes the text from textarea to the mentioned file.
-    private void writeTextFile (JTextArea display, String fileName)
-    {
+    //The method writes the content of the text area to a file and clears the input fields.
+    private void sendOperation(ActionEvent ae,String fileMentioned,JTextArea aj){
         try
         {
-            PrintWriter outStream =  new PrintWriter (new File (fileName));
-            outStream.print (display.getText());
+            PrintWriter outStream =  new PrintWriter (new File (fileMentioned));
+            outStream.print(aj.getText());
             outStream.close ();
+
+            setTitle("New Message");
+            textfield1.setText("");
+            textfield2.setText("");
+            textfield3.setText("");
+            textfield4.setText("");
+            area.setText("");
         }
         catch (IOException e)
         {
-            display.setText("IOERROR: " + e.getMessage() + "\n");
+            aj.setText("IOERROR: " + e.getMessage() + "\n");
             e.printStackTrace();
         }
-    } 
-
-    //The method updates the frames title based on the condition.
-    private void updateTitle() {
-
-         public void focusGained(FocusEvent e) {}
-
-        //When focus is lost the title is updated using submethod.
-        public void focusLost(FocusEvent e) {
-            updateTitle();   
-        }
-        String subject = textfield4.getText();
-        setTitle(subject.isEmpty() ? "New Message" :  subject);
     }
-
- */
+}
---
+++ TrafficLight.java
@@ -0,0 +1,66 @@
+
+/*
+ *@author: Henry Leitner
+ * @version: Last modified on April 04, 2024
+ */
+import javax.swing.*;
+import java.awt.*;
+
+//The class demonstrates the drawing of traffic lights using graphics in java.
+public class TrafficLight {
+    // The main method constructs a drawing object.
+    public static void main(String[] args) {
+        Drawing lights = new Drawing();
+        lights.setSize(127, 400);
+        lights.setTitle("Traffic Lights");
+        lights.setVisible(true);
+
+    }
+}
+
+// The class extends JFrame.
+class Drawing extends JFrame {
+    final int WIDTH = 120,
+            HEIGHT = 120;
+    private Color redShade = new Color(239, 1, 0);
+    private Color yellowShade = new Color(181, 163, 0);
+    private Color greenShade = new Color(76, 177, 23);
+
+    private MyDrawingPanel drawingPanel = new MyDrawingPanel(WIDTH, HEIGHT, redShade, yellowShade, greenShade);
+
+    public Drawing() {
+        add(drawingPanel);
+    };
+
+    private class MyDrawingPanel extends JPanel {
+        private int width, height;
+        private Color red;
+        private Color yellow;
+        private Color green;
+
+        public MyDrawingPanel() {
+        };
+
+        public MyDrawingPanel(int width, int height, Color red, Color yellow, Color green) {
+            this.width = width;
+            this.height = height;
+            this.red = red;
+            this.yellow = yellow;
+            this.green = green;
+        }
+
+        public void paintComponent(Graphics g) {
+
+            g.setColor(this.red);
+            g.drawOval(5, 5, this.width, this.height);
+            g.fillOval(5, 5, this.width, this.height);
+            g.setColor(this.yellow);
+            g.drawOval(5, 127, this.width, this.height);
+            g.fillOval(5, 127, this.width, this.height);
+            g.setColor(this.green);
+            g.drawOval(5, 249, this.width, this.height);
+            g.fillOval(5, 249, this.width, this.height);
+
+        }
+    }
+}
